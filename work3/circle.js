929ead9358e46c3f1a2dd4db301fc5e00bd60474
yangzhiyi-Yzy <1649895581@qq.com>
2019-10-11 20:52:38 +0800

    work3

--- work3/circle.js
+++ work3/circle.js
@@ -7,7 +7,8 @@ if (!isNaN(r)) {
     document.getElementById('cir').value = cir.toFixed(2)
     document.getElementById('area').value = area.toFixed(2)
 
-}
-else {
+} else {
     alert('请输入正确的数字')
 }
+
+
