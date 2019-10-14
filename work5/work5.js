dfa99d92403029a3c240389be1353a8918a4b13c
yangzhiyi-Yzy <1649895581@qq.com>
2019-10-14 16:42:22 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,36 +1,34 @@
-//9x9的空表
-    let str = '<table border="1">';
-    for (let i =1;i<10;++i){
-      str += '<tr>';
-      for(var j=1;j<=i;++j){
-          str += '<td>&nbsp;</td>'
-      }
-      str += '</tr>'
-    }
-    str += '</table>';
-    document.getElementById('table').innerHTML = str
-
+//9x9的空表，请自行编写这段代码
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+  str +='<tr>'
+  for(var j=9;j>0;--j){
+    str +='<td>&nbsp;</td>'
+  }
+  str +='</td>'
+}
+str +='</table>'
+document.getElementById('table1').innerHTML = str
 //倒九九乘法空表
-str2 = '<table border="1">'
+str ='<table border="1">'
 for(let i=9;i>0;--i){
-    str2 += '<tr>'
-    for(var j=i;j>0;--j){
-        str2 += '<td>&nbsp;</td>'
-    }
-    str2 +='</tr>'
+  str +='<tr>'
+  for(var j=i;j>0;--j){
+    str +='<td>&nbsp;</td>'
+  }
+  str +='</td>'
 }
-str2 += '</table>'
+str +='</table>'
 document.getElementById('table2').innerHTML = str
-
-//倒九九乘法表
-str3 = '<table borble="1">'
+//倒九九乘法表，请自行编写这段代码
+str ='<table border="1">'
 for(let i=9;i>0;--i){
-    str3 += '<tr>'
-    for(var j=i;j>0;--j){
-        str3 += '<td>' + j + '*' + i + '=' + ( j * i) + '</td>'
-    }
-    str3 +='</tr>'
+  str +='<tr>'
+  for(var j=i;j>0;--j){
+    str +='<td>' + j + '*' + i + '=' + ( j * i )+'</td>'
+  }
+  str +='</td>'
 }
-str3 += '<table>'
+str +='</table>'
 document.getElementById('table3').innerHTML = str
 
